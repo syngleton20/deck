738d372ca8fb1cc4209687ef2238ca5fc8b67f46
syngleton <>
2025-12-02 13:42:08 +0000

    Test bar hiding again and iOS theming

--- index.js
+++ index.js
@@ -5,41 +5,28 @@ function flipCard() {
 	card.innerHTML = flipped ? "Flipped successfully!" : "Testing, testing, 1, 2, 3...";
 }
 
+const themeSelect = document.getElementById("theme-select");
+function themeChange() {
+	console.log("test");
+	if (themeSelect.value === "dark")
+		document.querySelector("meta[name='theme-color']").setAttribute("content", "#121212");
+	if (themeSelect.value === "light")
+		document.querySelector("meta[name='theme-color']").setAttribute("content", "#cfcfcf");
+}
+
 /* Initialization */
+themeSelect.addEventListener("change", () => { themeChange() });
+
 window.addEventListener('touchstart', evt => { }, { passive: true });
 
-(function (win) {
-	var doc = win.document;
+var page = document.body;
+ua = navigator.userAgent,
+	iphone = ~ua.indexOf('iPhone') || ~ua.indexOf('iPod');
 
-	// If there's a hash, or addEventListener is undefined, stop here
-	if (!location.hash && win.addEventListener) {
-
-		//scroll to 1
-		win.scrollTo(0, 1);
-		var scrollTop = 1,
-			getScrollTop = function () {
-				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-			},
-
-			//reset to 0 on bodyready, if needed
-			bodycheck = setInterval(function () {
-				if (doc.body) {
-					clearInterval(bodycheck);
-					scrollTop = getScrollTop();
-					win.scrollTo(0, scrollTop === 1 ? 0 : 1);
-				}
-			}, 15);
-
-		win.addEventListener("load", function () {
-			setTimeout(function () {
-				//at load, if user hasn't scrolled more than 20 or so...
-				if (getScrollTop() < 20) {
-					//reset to hide addr bar at onload
-					win.scrollTo(0, scrollTop === 1 ? 0 : 1);
-				}
-			}, 0);
-		}, false);
-	}
-})(this);
-
-alert("Testing, testing, 1, 2, 3...");
+var setupScroll = window.onload = function () {
+	var height = document.documentElement.clientHeight;
+	if (iphone && !fullscreen) height += 60;
+	page.style.height = height + 'px';
+	setTimeout(scrollTo, 0, 0, 1);
+	this.alert("done");
+};
